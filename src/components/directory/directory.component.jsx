16834dbeb1f38fc6999fe107e5f99d5c9b5d1a18
david s <davoudsa@gmail.com>
2021-04-19 13:13:44 -0400

    second commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [{
+                title: 'For Sale',
+                imageUrl:'https://cdn0.zoocasa.com/images/listings/c951bc2a-8edb-44d5-9793-d6704e67474a.jpg',
+                id: 1
+            },{
+                title: 'For Rent',
+                imageUrl:'https://cdn3.zoocasa.com/images/listings/0b3609ea-2789-442b-90b0-d4268af2d9c6.jpg',
+                id: 2
+            },{
+                title: 'Sold',
+                imageUrl:'https://cdn0.zoocasa.com/images/listings/0325c4fb-0963-4255-94b6-9947722afdeb.jpg',
+                id: 3
+            },{
+                title: 'Market stats',
+                imageUrl:'https://www.zoocasa.com/blog/wp-content/uploads/2021/04/TREB-March-2021-Home-Prices-By-Quarter-Hero-750x429.png',
+                size: 'large',
+                id: 4
+            },{
+                title: 'Featured Homes',
+                imageUrl: 'https://cdn1.zoocasa.com/images/listings/4f89778b-4d19-4bce-b139-0bc4d9d277f9.jpg',
+                size: 'large',
+                id: 5
+            }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'> 
+            {
+                this.state.sections.map(({title, imageUrl, id, size}) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    )
+                    )
+            }
+            </div>
+        )
+    }
+}
+
+export default Directory;
